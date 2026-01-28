f8cf1bec9f86d97afb454cb3815ad7565dd2c0b0
Timmy20s <toseenmotunbade20@gmail.com>
2026-01-28 11:43:41 +0100

    created a method that would receive a list of Posts

--- lib/repository/post_repository.dart
+++ lib/repository/post_repository.dart
@@ -1,3 +1,16 @@
+import 'dart:convert';
+
+import 'package:remote_server_demo/model/post.dart';
+import 'package:http/http.dart';
+
 class PostRepository{
   final String baseUrl = "https://jsonplaceholder.typicode.com";
+
+  Future<List<Post>> fetchPosts() async{
+    final response = await http.get(Uri.parse('$baseUrl/posts'));
+
+    if(response.statusCode == 200){
+      List<dynamic> body = jsonDecode(response.body);
+    }
+  }
 }
